7a2cd023216c8657f50a4a2fbc104b8016fe6018
wojtin <wojtin@o2.pl>
2023-07-18 22:32:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import SimpleITK as sitk
 app = Flask(__name__)
+
+print(sitk.Version())
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World2!'
+    return sitk.Version()
